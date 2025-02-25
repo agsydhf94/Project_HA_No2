d9ce519ba932d58018383b17f32d9ebf5cb712a3
DESKTOP-D91TOJO\c <agsydhf94@gmail.com>
2025-02-26 05:35:18 +0900

    캐릭터 베이스와 데미지 인터페이스 준비

---
+++ Assets/Project_HA_No2/Scripts/Character/CharacterBase.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HA
+{
+    public class CharacterBase : MonoBehaviour, IDamagable
+    {
+        public void ApplyDamage(float damage)
+        {
+            
+        }
+    }
+}

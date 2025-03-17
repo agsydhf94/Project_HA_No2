22f22666e89bbeb5f6fbf2e9399c6a15dad77636
YoungHoon Kang <agsydhf94@gmail.com>
2025-03-17 23:52:18 +0900

    플레이어 캐릭터 클론의 Material을 Transparent 하게 만드는 작업 중 ( 일부 Material의 Rendering-Alpha Mode를 Transparent로 설정하는 데 성공 )

--- Assets/Project_HA_No2/Scripts/Skill/SkillController/CloneSkillController.cs
+++ Assets/Project_HA_No2/Scripts/Skill/SkillController/CloneSkillController.cs
@@ -15,19 +15,62 @@ namespace HA
         [SerializeField] private float attackCheckRadius;
         private Transform closestEnemy;
 
-        
+        [SerializeField] private SkinnedMeshRenderer smr_Face;
+        [SerializeField] private SkinnedMeshRenderer smr_Body;
+        [SerializeField] private SkinnedMeshRenderer smr_Hair;
+        [SerializeField] private float colorLoosingSpeed;
+
+
 
         private void Awake()
         {
             animator = GetComponent<Animator>();
+
+            SetTransparentAllMaterials(transform, smr_Face);
+            SetTransparentAllMaterials(transform, smr_Body);
+            SetTransparentAllMaterials(transform, smr_Hair);
         }
 
         private void Update()
         {
             cloneTimer -= Time.deltaTime;
+
+            if (cloneDuration <= 0)
+            {
+                ReduceAlphaAllMaterials(smr_Face, colorLoosingSpeed);
+                ReduceAlphaAllMaterials(smr_Body, colorLoosingSpeed);
+                ReduceAlphaAllMaterials(smr_Hair, colorLoosingSpeed);
+            }
         }
 
-        
+        void SetTransparentAllMaterials(Transform trasnform, SkinnedMeshRenderer smr)
+        {
+            for (int i = 0; i < smr.materials.Length; i++)
+            {
+                Material mat = new Material(smr.materials[i]); // 개별 인스턴스화
+                mat.SetFloat("_AlphaMode", 2);  // 2 = Transparent (수동 설정)
+                mat.SetOverrideTag("RenderType", "Transparent");
+                mat.renderQueue = 3000; // 투명 오브젝트 렌더링 순서
+                mat.SetInt("_SrcBlend", (int)UnityEngine.Rendering.BlendMode.SrcAlpha);
+                mat.SetInt("_DstBlend", (int)UnityEngine.Rendering.BlendMode.OneMinusSrcAlpha);
+                mat.SetInt("_ZWrite", 0);  // 깊이 쓰기 비활성화 (투명 처리 가능)
+
+                smr.materials[i] = mat;  // 적용
+            }
+        }
+
+        void ReduceAlphaAllMaterials(SkinnedMeshRenderer smr, float fadeSpeed)
+        {
+            for (int i = 0; i < smr.materials.Length; i++)
+            {
+                Material mat = smr.materials[i];
+                Color color = mat.color;
+
+                color.a -= cloneTimer * fadeSpeed;
+                mat.color = color;
+            }
+        }
+
 
         public void SetUpClone(Transform newTransform, bool canAttack)
         {

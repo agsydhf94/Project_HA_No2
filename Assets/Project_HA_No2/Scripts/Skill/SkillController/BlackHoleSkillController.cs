22abf6767c522be1495fbfde5592dbffafe631ec
YoungHoon Kang <agsydhf94@gmail.com>
2025-04-03 01:51:42 +0900

    블랙홀 구체 스킬 - 블랙홀 정보 초기화 및 타이머 세팅

--- Assets/Project_HA_No2/Scripts/Skill/SkillController/BlackHoleSkillController.cs
+++ Assets/Project_HA_No2/Scripts/Skill/SkillController/BlackHoleSkillController.cs
@@ -8,38 +8,99 @@ namespace HA
     public class BlackHoleSkillController : MonoBehaviour
     {
         [SerializeField] private GameObject attackUIPrefab;
+        private SkillManager skillManager;
 
-        public float maxSize;
-        public float growSpeed;
-        public bool canGrow;
+        private float maxSize;
+        private float growSpeed;
+        private float shrinkSpeed;
+
+        private bool canGrow;
+        private bool canShrink;
+        private bool canAttack;
+
+        public int amountOfAttacks = 4;
+        public float cloneAttackCooldown = 0.3f;
+        private float cloneAttackTimer;
 
         public List<Enemy> attackedTargets = new List<Enemy>();
         public Queue<Enemy> detectedTargets = new Queue<Enemy>();
 
+        public void SetupBlackHole(float maxSize, float growSpeed, float shrinkSpeed, int amountOfAttacks, float cloneAttackCooldown)
+        {
+            this.maxSize = maxSize;
+            this.growSpeed = growSpeed;
+            this.shrinkSpeed = shrinkSpeed;
+            this.amountOfAttacks = amountOfAttacks;
+            this.cloneAttackCooldown = cloneAttackCooldown;
+        }
+
+        private void Awake()
+        {
+            skillManager = SkillManager.Instance;
+        }
+
         private void Update()
         {
-            transform.localScale
-                = Vector3.Lerp(transform.localScale, new Vector3(maxSize, maxSize, maxSize), growSpeed * Time.deltaTime);
+            cloneAttackTimer -= Time.deltaTime;
 
+            if (cloneAttackTimer < 0)
+            {
+                cloneAttackTimer = cloneAttackCooldown;
+            }
+
+            if (canGrow && !canShrink)
+            {
+                transform.localScale
+                = Vector3.Lerp(transform.localScale, new Vector3(maxSize, maxSize, maxSize), growSpeed * Time.deltaTime);
+            }
+            if (canShrink)
+            {
+                transform.localScale
+                = Vector3.Lerp(transform.localScale, new Vector3(-1, -1, -1), shrinkSpeed * Time.deltaTime);
+
+                if (transform.localScale.x < 0)
+                {
+                    Destroy(gameObject);
+                }
+            }
+
+            AttackByClone();
+        }
+
+        private void AttackByClone()
+        {
             if (Input.GetKeyDown(KeyCode.Z))
             {
                 var enemy = detectedTargets.Dequeue();
                 AddEnemyToList(enemy);
                 enemy.blackHoleFlag.SetActive(false);
+                skillManager.cloneSkill.CreateClone(enemy.transform, Vector3.zero);
+
+                amountOfAttacks--;
+                if (amountOfAttacks <= 0)
+                {
+                    canAttack = false;
+                    canShrink = true;
+                }
             }
         }
 
         private void OnTriggerEnter(Collider other)
         {
-            if (other.TryGetComponent<Enemy>(out Enemy enemy))
+            if (other.TryGetComponent(out Enemy enemy))
             {
                 enemy.FreezeTime(true);
                 enemy.blackHoleFlag.SetActive(true);
                 detectedTargets.Enqueue(enemy);
                 Debug.Log(enemy.gameObject.name);
+            }
+        }
 
-                BlackHoleSkill_AttackIcon attackIcon = enemy.blackHoleFlag.gameObject.GetComponent<BlackHoleSkill_AttackIcon>();
-                attackIcon.SetupHotKey(hotkey, other.transform, this);
+        private void OnTriggerExit(Collider other)
+        {
+            if(other.TryGetComponent(out Enemy enemy))
+            {
+                enemy.FreezeTime(false);
             }
         }
 

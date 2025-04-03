03155b502295e614f274c9d02999c1ae63699b7b
YoungHoon Kang <agsydhf94@gmail.com>
2025-04-03 19:07:40 +0900

    블랙홀 구체 스킬 프리팹 초기화 로직

---
+++ Assets/Project_HA_No2/Scripts/Skill/Skills/BlackHoleSkill.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HA
+{
+    public class BlackHoleSkill : Skill
+    {
+        [SerializeField] private int amountOfAttacks;
+        [SerializeField] private float cloneCoolTime;
+        [SerializeField] private GameObject blackHoldPrefab;
+        [SerializeField] private float maxSize;
+        [SerializeField] private float growSpeed;
+        [SerializeField] private float shrinkSpeed;
+
+        private BlackHoleSkillController currentBlackHole;
+
+        public override bool CanUseSkill()
+        {
+            return base.CanUseSkill();
+        }
+
+        public override void UseSkill()
+        {
+            base.UseSkill();
+
+            currentBlackHole = Instantiate(blackHoldPrefab).GetComponent<BlackHoleSkillController>();
+            currentBlackHole.transform.SetParent(playerCharacter.transform);
+            currentBlackHole.transform.localPosition = Vector3.zero;
+
+            var controller = currentBlackHole.GetComponent<BlackHoleSkillController>();
+            controller.SetupBlackHole(maxSize, growSpeed, shrinkSpeed, amountOfAttacks, cloneCoolTime);
+        }
+
+        protected override void Start()
+        {
+            base.Start();
+        }
+
+        protected override void Update()
+        {
+            base.Update();
+        }
+
+        public bool BlackHoleFinished()
+        {
+            if (!currentBlackHole)
+                return false;
+
+            if(currentBlackHole.playerCanExitState)
+            {
+                currentBlackHole = null;
+                return true;
+            }
+            return false;
+        }
+    }
+}

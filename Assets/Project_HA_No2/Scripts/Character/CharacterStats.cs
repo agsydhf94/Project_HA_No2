e126d0a35c500707b0769d84b5c82a9a8d062cda
YoungHoon Kang <agsydhf94@gmail.com>
2025-04-08 02:04:33 +0900

    evasion, agility 스탯정보에 의한 공격 회피효과 구현

--- Assets/Project_HA_No2/Scripts/Character/CharacterStats.cs
+++ Assets/Project_HA_No2/Scripts/Character/CharacterStats.cs
@@ -1,14 +1,21 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace HA
 {
     public class CharacterStats : MonoBehaviour
     {
-        public Stat strength;
-        public Stat damage;
+        [Header("Main Stats")]
+        public Stat strength;     // 1 point : 공격 데미지 +1, 크리티컬 파워 +1%
+        public Stat agility;      // 1 point : 회피 증가, 크리티컬 회피 +1%
+        public Stat inteligence;  // 1 point : 마법 데미지 +1, 마법 저항력 + 3%
+        public Stat vitality;     // 1 point : 최대 체력 +5
+
+        [Header("Defense Stats")]
         public Stat maxHp;
+        public Stat armor;
+        public Stat evasion;
+
+        public Stat damage;
 
         [SerializeField] private int currentHp;
 
@@ -19,6 +26,14 @@ namespace HA
 
         public virtual void DoDamage(CharacterStats targetStats)
         {
+            int totalEvasion = targetStats.evasion.GetValue() + targetStats.agility.GetValue();
+
+            if(Random.Range(1, 100) < totalEvasion)
+            {
+                Debug.Log("Attack Avoided");
+                return;
+            }
+
             int totalDamage = damage.GetValue() + strength.GetValue();
             targetStats.TakeDamage(totalDamage);
         }

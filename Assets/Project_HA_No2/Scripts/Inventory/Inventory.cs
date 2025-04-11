298c00f23c99709a26bcc17387a21cc72ad70d9d
YoungHoon Kang <agsydhf94@gmail.com>
2025-04-11 23:54:18 +0900

    Inventory 클래스에서 아이템 추가 및 제거 로직 작성

--- Assets/Project_HA_No2/Scripts/Inventory/Inventory.cs
+++ Assets/Project_HA_No2/Scripts/Inventory/Inventory.cs
@@ -6,11 +6,43 @@ namespace HA
 {
     public class Inventory : SingletonBase<Inventory>
     {
-        public List<ItemDataSO> inventory = new List<ItemDataSO>();
+        public List<InventoryItem> inventoryItems;
+        public Dictionary<ItemDataSO, InventoryItem> inventoryDictionary;
+
+        private void Start()
+        {
+            inventoryItems = new List<InventoryItem>();
+            inventoryDictionary = new Dictionary<ItemDataSO, InventoryItem>();
+        }
 
         public void AddItem(ItemDataSO item)
         {
-            inventory.Add(item);
+            if(inventoryDictionary.TryGetValue(item, out InventoryItem value))
+            {
+                value.AddStack();
+            }
+            else
+            {
+                InventoryItem newItem = new InventoryItem(item);
+                inventoryItems.Add(newItem);
+                inventoryDictionary.Add(item, newItem);
+            }
+        }
+
+        public void RemoveItem(ItemDataSO item)
+        {
+            if(inventoryDictionary.TryGetValue(item, out InventoryItem value))
+            {
+                if(value.stackSize <= 1)
+                {
+                    inventoryItems.Remove(value);
+                    inventoryDictionary.Remove(item);
+                }
+                else
+                {
+                    value.RemoveStack();
+                }
+            }
         }
     }
 }

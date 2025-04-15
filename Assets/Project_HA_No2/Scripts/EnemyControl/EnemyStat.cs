f68faf653256a5e0c1bb2f475ad0f446dc17706e
YoungHoon Kang <agsydhf94@gmail.com>
2025-04-15 22:43:17 +0900

    적의 레벨에 따른 적의 능력치를 Modify할 수 있는 로직 작성

--- Assets/Project_HA_No2/Scripts/EnemyControl/EnemyStat.cs
+++ Assets/Project_HA_No2/Scripts/EnemyControl/EnemyStat.cs
@@ -8,10 +8,44 @@ namespace HA
     {
         private Enemy enemy;
 
+        [Header("Level information")]
+        [SerializeField] private int level;
+
+        [Range(0f, 1f)]
+        [SerializeField] private float percentageModifier;
+
         protected override void Start()
         {
+            ApplyModifiers();
+
             base.Start();
             enemy = GetComponent<Enemy>();
+        }
+
+        private void ApplyModifiers()
+        {
+            Modify(damage);
+            Modify(criticalChance);
+            Modify(criticalPower);
+
+            Modify(maxHp);
+            Modify(armor);
+            Modify(evasion);
+            Modify(magicResistance);
+
+            Modify(fireDamage);
+            Modify(iceDamage);
+            Modify(lightingDamage);
+        }
+
+        private void Modify(Stat stat)
+        {
+            for(int i = 0; i < level; i++)
+            {
+                float modifier = stat.GetValue() * percentageModifier;
+
+                stat.AddModifier(Mathf.RoundToInt(modifier));
+            }
         }
 
         public override void TakeDamage(int _damage)

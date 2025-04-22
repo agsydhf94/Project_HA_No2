fa02a6ffd036e158bc5305f2837d3767dc18eaf8
YoungHoon Kang <agsydhf94@gmail.com>
2025-04-22 11:39:54 +0900

    자신으로부터 가장 가까이에 있는 적을 가져오는 정적 메서드 구현

---
+++ Assets/Project_HA_No2/Scripts/FunctionClasses/FindClosestEnemy.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HA
+{
+    public static class FindClosestEnemy
+    {
+        public static Transform GetClosestEnemy(Transform self, bool excludeSelf = false)
+        {
+            List<Collider> coliiders = CharacterBase.ObjectDetection<Enemy>(self, 5f);
+
+            float closestDistance = Mathf.Infinity;
+            Transform closestEnemy = null;
+
+            foreach (var collider in coliiders)
+            {
+                if (excludeSelf && collider.transform == self)
+                    continue;
+
+                float distanceToEnemy = Vector3.Distance(self.position, collider.transform.position);
+
+                if (distanceToEnemy < closestDistance)
+                    closestDistance = distanceToEnemy;
+                closestEnemy = collider.transform;
+            }
+
+            if(closestEnemy != null)
+            {
+                return closestEnemy;
+            }
+            else
+            {
+                return null;
+            }
+            
+        }
+    }
+}

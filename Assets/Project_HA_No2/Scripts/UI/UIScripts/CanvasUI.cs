a3cabf0f0f318670cdbe3b0570303b6dc9a0f91f
YoungHoon Kang <agsydhf94@gmail.com>
2025-04-25 23:53:15 +0900

    키를 입력하면 UI의 각 Panel을 Switch할 수 있게 구현

--- Assets/Project_HA_No2/Scripts/UI/UIScripts/CanvasUI.cs
+++ Assets/Project_HA_No2/Scripts/UI/UIScripts/CanvasUI.cs
@@ -6,8 +6,41 @@ namespace HA
 {
     public class CanvasUI : MonoBehaviour
     {
+        [SerializeField] private GameObject characterUI;
+        [SerializeField] private GameObject skillTreeUI;
+        [SerializeField] private GameObject craftUI;
+        [SerializeField] private GameObject optionsUI;
+
         public ItemToolTipUI itemToolTipUI;
         public StatToolTipUI statToolTipUI;
+
+        private void Start()
+        {
+            SwitchUI(null);
+
+            itemToolTipUI.gameObject.SetActive(false);
+            statToolTipUI.gameObject.SetActive(false);
+        }
+
+        private void Update()
+        {
+            if(Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                SwitchUITo(characterUI);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                SwitchUITo(skillTreeUI);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha3))
+            {
+                SwitchUITo(craftUI);
+            }
+            if (Input.GetKeyDown(KeyCode.Alpha4))
+            {
+                SwitchUITo(optionsUI);
+            }
+        }
 
         public void SwitchUI(GameObject menu)
         {
@@ -20,6 +53,17 @@ namespace HA
             {
                 menu.SetActive(true);
             }
+        }
+
+        public void SwitchUITo(GameObject menuUI)
+        {
+            if(menuUI != null && menuUI.activeSelf)
+            {
+                menuUI.SetActive(false);
+                return;
+            }
+
+            SwitchUI(menuUI);
         }
     }
 }

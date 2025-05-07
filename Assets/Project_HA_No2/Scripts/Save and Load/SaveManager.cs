e0808cea9d2fa839865fd31b5ff8ad86f3e3ba9b
YoungHoon Kang <agsydhf94@gmail.com>
2025-05-07 23:27:52 +0900

    fix : ensure game data is loaded correctly after scene transition

--- Assets/Project_HA_No2/Scripts/Save and Load/SaveManager.cs
+++ Assets/Project_HA_No2/Scripts/Save and Load/SaveManager.cs
@@ -2,12 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using UnityEngine.SceneManagement;
 
 namespace HA
 {
     public class SaveManager : SingletonBase<SaveManager>
     {
         [SerializeField] private string fileName;
+
+        private readonly HashSet<string> scenesToLoad 
+            = new HashSet<string> { "TestScene" };
 
         private GameData gameData;
         private List<ISaveManager> saveManagers;
@@ -17,9 +21,32 @@ namespace HA
         {
             dataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
             saveManagers = FindAllSaveManagers();
-
-            LoadGame();
         }
+
+        #region Scene Information Refresh
+
+        private void OnEnable()
+        {
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
+
+        private void OnDisable()
+        {
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+        }
+
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            Debug.Log($"¾À ·ÎµåµÊ: {scene.name}");
+
+            if (scenesToLoad.Contains(scene.name))
+            {
+                RefreshSaveManagers();
+                LoadGame();
+            }
+        }
+
+        #endregion
 
         public void NewGame()
         {
@@ -58,8 +85,15 @@ namespace HA
             return new List<ISaveManager>(saveManagers);
         }
 
+        
+
+        public void RefreshSaveManagers()
+        {
+            saveManagers = FindAllSaveManagers();
+        }
+
         [ContextMenu("Delete Save File")]
-        private void DeleteSaveData()
+        public void DeleteSaveData()
         {
             dataHandler = new FileDataHandler(Application.persistentDataPath, fileName);
             dataHandler.DeleteData();
